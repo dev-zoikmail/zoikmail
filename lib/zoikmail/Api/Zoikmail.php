695a1b3fdac02ccba937219120103eed41a26448
dev-zoikmail <pradeep@zoikmail.com>
2016-01-11 17:09:38 +0530

    Create Zoikmail.php

---
+++ lib/zoikmail/Api/Zoikmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Zoikmail\Api;
+
+use Zoikmail\HttpClient\HttpClient;
+
+class Zoikmail
+{
+
+    /**
+     * @var HttpClient
+     */
+    private $client;
+
+    /**
+     * @param HttpClient $client
+     */
+    public function __construct(HttpClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Email Verification
+     *
+     * '/verify?email=:email&timeout=:timeout' GET
+     *
+     * @param string $email Email address to verify
+     * @param array $options
+     * @return \Zoikmail\HttpClient\Response
+     */
+    public function verify($email, array $options = array())
+    {
+        $body = (isset($options['query']) ? $options['query'] : array());
+
+        $timeout = (isset($options['timeout']) ? $options['timeout'] : 60000);
+
+        $response = $this->client->get('/verify?email='.rawurlencode($email).'', $body, $options);
+
+        return $response;
+    }
+
+}
+?>

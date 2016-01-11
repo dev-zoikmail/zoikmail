21875d6cab4cc647de3f38bd17056a8ec6a9559c
dev-zoikmail <pradeep@zoikmail.com>
2016-01-11 17:08:16 +0530

    Create ClientException.php

---
+++ lib/zoikmail/Exception/ClientException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zoikmail\Exception;
+
+/**
+ * ClientException is used when the api returns an error
+ */
+class ClientException extends \ErrorException implements ExceptionInterface
+{
+
+    public $code = null;
+
+    public function __construct($message, $code) {
+        $this->code = $code;
+        parent::__construct($message);
+    }
+
+}
+?>
